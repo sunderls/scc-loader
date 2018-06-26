3957f42da55d6b4adb1623651681f9ff9860eb35
sunderls <sunderls@126.com>
2018-06-27 00:41:54 +0900

    Add test for single component

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,13 +1,41 @@
-const loader = require('../index');
+import sccLoader from '../index';
+import Module from 'module';
+import * as babel from 'babel-core';
+import * as path from 'path';
+import styled from 'styled-components';
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
 
-test('it component', () => {
-  const str = loader(`
-    Title {
-      component: h1;
+const expectSameComponents = (A, B) => {
+  expect(A.componentStyle.rules).toEqual(B.componentStyle.rules);
+};
+
+const getModuleFromString = str => {
+  const codeES5 = babel.transform(str, {
+    presets: ['env'],
+    plugins: ['babel-plugin-styled-components']
+  }).code;
+  const m = new Module();
+  m.paths = Module._nodeModulePaths(path.dirname(''));
+  m._compile(codeES5, '');
+  return m.exports.default;
+};
+
+test('transofrm single component', () => {
+  expectSameComponents(
+    styled.h1`
       font-size: 1.5em;
       text-align: center;
-    }
-  `);
-  expect(str).toContain(`import styled from 'styled-components';`);
-  expect(str).toContain('Title: styled.h1`font-size:1.5em;text-align:center;`');
+    `,
+
+    getModuleFromString(
+      sccLoader(`
+      Title {
+        component: h1;
+        font-size: 1.5em;
+        text-align: center;
+      }
+    `)
+    ).Title
+  );
 });
