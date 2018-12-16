1e54bec6875605163e9691e0540cf8937a5ce01c
sunderls <sunderls@126.com>
2018-12-17 01:18:31 +0900

    default tag and calc()

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,38 +1,42 @@
 import sccLoader from '../index';
+import React from 'react';
 import styled from 'styled-components';
 import { expectSameComponents, getModuleFromString } from './util';
+import { create } from 'react-test-renderer';
+import 'jest-styled-components';
 
-test('transform single component', () => {
-  expectSameComponents(
-    styled.h1`
-      font-size: 1.5em;
-      text-align: center;
-    `,
-
-    getModuleFromString(
-      sccLoader(`
+test('should support new rule: component ', () => {
+  const SccTitle = getModuleFromString(
+    sccLoader(`
       Title {
         component: h1;
-        font-size: 1.5em;
-        text-align: center;
       }
     `)
-    ).Title
-  );
+  ).Title;
+  const tree = create(<SccTitle>title</SccTitle>).toJSON();
+  expect(tree.type).toBe('h1');
 });
 
-test('default component should be div', () => {
-  expectSameComponents(
-    styled.div`
-      font-size: 1.5em;
-    `,
-
-    getModuleFromString(
-      sccLoader(`
+test('rule: component should have default value of div', () => {
+  const SccTitle = getModuleFromString(
+    sccLoader(`
       Title {
-        font-size: 1.5em;
       }
     `)
-    ).Title
-  );
+  ).Title;
+  const tree = create(<SccTitle>title</SccTitle>).toJSON();
+  expect(tree.type).toBe('div');
+});
+
+test('should transform to styled component', () => {
+  const transformedCode = sccLoader(`
+    Title {
+      component: h3;
+      font-size: calc((props) => props.size);
+      color: red;
+    }
+  `);
+  expect(transformedCode).toContain('styled.h3');
+  expect(transformedCode).toContain('font-size:${(props)=>props.size}');
+  expect(transformedCode).toContain('color:red;');
 });
--- __test__/util.js
+++ __test__/util.js
@@ -1,17 +1,12 @@
 import Module from 'module';
 import * as babel from 'babel-core';
 import * as path from 'path';
-
-export const expectSameComponents = (A, B) => {
-  expect(A.target).toEqual(B.target);
-  expect(A.componentStyle.rules).toEqual(B.componentStyle.rules);
-};
-
 export const getModuleFromString = str => {
-  const codeES5 = babel.transform(str, {
-    presets: ['env'],
-    plugins: ['babel-plugin-styled-components']
-  }).code;
+  const codeES5 = [
+    babel.transformSync(str, {
+      presets: ['@babel/preset-env']
+    }).code
+  ].join('\n');
   const m = new Module();
   m.paths = Module._nodeModulePaths(path.dirname(''));
   m._compile(codeES5, '');
